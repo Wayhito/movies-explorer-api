eb59820988501e74c2269aa0783394f8106da967
Wayhito <moskariko@mail.ru>
2023-08-16 12:24:06 +0200

    auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,25 +3,22 @@ const UnauthorizedError = require('../errors/Unauthorized');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-function auth(req, res, next) {
+module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secretkey');
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'secretkey'}`);
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError('Необходима авторизация');
   }
-
   req.user = payload;
 
-  return next();
-}
-
-module.exports = { auth };
+  next();
+};
