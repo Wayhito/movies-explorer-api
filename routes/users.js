5cfe6cafa6b850e5e13d85073f887523415d3dfa
Wayhito <moskariko@mail.ru>
2023-08-16 20:30:39 +0200

    validation userid check

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,7 @@ const { getCurrentUserInfo, setUserInfo } = require('../controllers/users');
 
 const users = express.Router();
 
-users.get(
-  '/me',
-  celebrate({
-    params: Joi.object().keys({
-      userId: Joi.length(24).hex(),
-    }),
-  }),
-  getCurrentUserInfo,
-);
+users.get('/me', getCurrentUserInfo);
 
 users.patch(
   '/me',
