ad076aa214f0f73e83004c2396f35370b2dc9a62
Wayhito <moskariko@mail.ru>
2023-08-17 15:33:05 +0200

    register user func

--- controllers/users.js
+++ controllers/users.js
@@ -10,56 +10,51 @@ const ConflictError = require('../errors/ConflictError');
 const UnauthorizedError = require('../errors/Unauthorized');
 const NotFoundError = require('../errors/NotFound');
 
-// async function registerUser(req, res, next) {
-//   try {
-//     const { email, password, name } = req.body;
-//     const passwordHash = await bcrypt.hash(password, 10);
-
-//     let user = await User.create({
-//       email,
-//       password: passwordHash,
-//       name,
-//     });
-
-//     user = user.toObject();
-//     delete user.password;
-//     res.status(201).send(user);
-//   } catch (err) {
-//     if (err.name === 'ValidationError') {
-//       next(new ValidationError('Неверные данные в  запросе'));
-//       return;
-//     }
-//     if (err.code === 11000) {
-//       next(new ConflictError('Пользователь с таким email уже существует'));
-//       return;
-//     }
-
-//     next(err);
-//   }
-// }
-
 const registerUser = (req, res, next) => {
   const { name, email, password } = req.body;
 
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name, email, password: hash,
-    }))
-    .then((({ _id }) => User.findById(_id)))
-    .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new ValidationError('Неверные данные в  запросе'));
-        return;
-      }
-      if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже существует'));
-        return;
-      }
-      next(err);
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({ name, email, password: hash }))
+    .then((user) => {
+      res.status(201).send({
+        name: user.name,
+        email: user.email,
+      });
     })
-    .catch(next);
+    .catch((err) => {
+      if (err.code === 11000) {
+        return next(new ConflictError('Пользователь с таким email уже существует'));
+      }
+      if (err.name === 'ValidationError') {
+        return next(new ValidationError('Неверные данные в  запросе'));
+      }
+      return next(err);
+    });
 };
+
+// const registerUser = (req, res, next) => {
+//   const { name, email, password } = req.body;
+
+//   bcrypt.hash(password, 10)
+//     .then((hash) => User.create({
+//       name, email, password: hash,
+//     }))
+//     .then((({ _id }) => User.findById(_id)))
+//     .then((user) => res.send(user))
+//     .catch((err) => {
+//       if (err.name === 'ValidationError') {
+//         next(new ValidationError('Неверные данные в  запросе'));
+//         return;
+//       }
+//       if (err.code === 11000) {
+//         next(new ConflictError('Пользователь с таким email уже существует'));
+//         return;
+//       }
+//       next(err);
+//     })
+//     .catch(next);
+// };
 
 async function loginUser(req, res, next) {
   try {
