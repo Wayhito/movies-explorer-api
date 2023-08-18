9c0958010036217c283d353c7a328d0b8fc147c1
Wayhito <moskariko@mail.ru>
2023-08-18 18:22:23 +0200

    console log deleted

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,8 @@ const UnauthorizedError = require('../errors/Unauthorized');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-function GFG(txt) {
-  console.log(txt);
-}
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  GFG({ authorization });
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация');
@@ -20,7 +15,6 @@ module.exports = (req, res, next) => {
 
   try {
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'secretkey'}`);
-    GFG(payload);
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
